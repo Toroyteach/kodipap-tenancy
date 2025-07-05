54722761da661493bf7304274924715e5988e5f8
Toroyteach <tonny35toro@gmail.com>
2025-07-05 18:51:34 +0300

    fix import files

--- resources/js/Pages/Multitenancy/Settings/appearance.jsx
+++ resources/js/Pages/Multitenancy/Settings/appearance.jsx
@@ -1,10 +1,8 @@
 import { Head } from '@inertiajs/react';
 
-import AppearanceTabs from '@/components/appearance-tabs';
-import HeadingSmall from '@/components/heading-small';
 
 import LandlordAppLayout from '@/Layouts/Landlord/landlordlayout';
-import SettingsLayout from '@/layouts/settings/layout';
+
 
 const breadcrumbs = [
     {
@@ -18,12 +16,7 @@ export default function Appearance() {
         <LandlordAppLayout breadcrumbs={breadcrumbs}>
             <Head title="Appearance settings" />
 
-            <SettingsLayout>
-                <div className="space-y-6">
-                    <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
-                    <AppearanceTabs />
-                </div>
-            </SettingsLayout>
+
         </LandlordAppLayout>
     );
 }
